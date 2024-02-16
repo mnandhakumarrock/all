d1d249018dc8993008c07eae275643b978b0fae7
“mnandhakumarrock” <mnandhakumarrock@gmail.com>
2024-02-16 12:26:33 +0000

    2d commit

--- main.tf
+++ main.tf
@@ -36,3 +36,11 @@ resource "aws_vpc" "vpc-ap-south-1" {
   }
 }
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.2.0.0/16"
+
+  tags = {
+    Name = "Main"
+  }
+}
