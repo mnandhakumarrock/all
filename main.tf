867277fde041a7f706089bdf558068dde49b5da8
“mnandhakumarrock” <mnandhakumarrock@gmail.com>
2024-02-16 11:46:17 +0000

    2d commit

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ provider "aws" {
 
 # Resource Block to Create VPC in us-east-1 which uses default provider
 resource "aws_vpc" "vpc-us-east-1" {
-  cidr_block = "10.1.5.0/16"
+  cidr_block = "10.2.0.0/16"
   tags = {
     "Name" = "vpc-us-east-1"
   }
 }
 
 resource "aws_vpc" "vpc-ap-south-1" {
-  cidr_block = "10.1.5.0/16"
+  cidr_block = "10.2.0.0/16"
   provider = aws.my-south
   tags = {
     "Name" = "vpc-ap-south-1"
